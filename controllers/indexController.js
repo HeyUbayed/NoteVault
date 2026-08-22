3313aca855d824bb5a1ae1929be25dc36a1e7506
HeyUbayed <ubaid.8r@gmail.com>
2026-08-22 11:11:46 +0600

    Update indexController.js

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,16 +1,21 @@
 const Note = require('../models/Note');
+const User = require('../models/User');
 
 exports.home = async (req, res, next) => {
     try {
-        const [departments, latest, stats] = await Promise.all([
+        const [departments, latest, topRated, trending, stats, topContributors, popularCourses] = await Promise.all([
             Note.getPopularDepartments(6),
             Note.getLatest(8),
-            Note.getGlobalStats()
+            Note.getTopRated(8),
+            Note.getTrending(8),
+            Note.getGlobalStats(),
+            User.getTopContributors(5),
+            Note.getPopularCourses(6)
         ]);
 
         res.render('home', {
             title: 'NoteVault — Share Knowledge, Ace Together',
-            departments, latest, stats
+            departments, latest, topRated, trending, stats, topContributors, popularCourses
         });
     } catch (err) {
         next(err);
