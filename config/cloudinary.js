77206b5d9090c0c6e0956946e4885419f149d16a
HeyUbayed <ubaid.8r@gmail.com>
2026-08-28 02:34:59 +0600

    Add uploadToCloudinary function
    Added function to upload memory buffer to Cloudinary.

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,31 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-module.exports = cloudinary;
+/**
+ * Upload memory buffer directly to Cloudinary
+ */
+const uploadToCloudinary = (fileBuffer, folder, resourceType = 'auto') => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: folder,
+        resource_type: resourceType
+      },
+      (error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      }
+    );
+    uploadStream.end(fileBuffer);
+  });
+};
+
+module.exports = {
+  cloudinary,
+  uploadToCloudinary
+};
